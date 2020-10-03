1e664cce539b549f99f93f459941f5d5d9e3e039
beaudoco <beaudoco@mail.gvsu.edu>
2020-10-02 21:56:31 -0400

    updating comments to match what program does

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 /*********************************************************
-This program takes a given filename, and sentence length
-and processes it to find the overall complexity of the
-articles language.
+This program takes two given file names and processes
+them to find the highest match score between the two
+strings. It outputs the resulting array to a text file
+so it can be compared to other outputs.
 
 @author: Collin Beaudoin
-@version: September 2020
+@version: October 2020
 *********************************************************/
 
 #include <stdio.h>
@@ -28,10 +29,10 @@ This is the main function of the code, it is used to
 accept the parameters that shall be used throughout the
 program.
 
-@parameter filename: This is used to select the file that
-will be checked for complexity
-@parameter sentenceLen: This is used to figure out the
-length to test for local complexity
+@parameter mainFile: This is used to select the file that
+will be parsed against to see if it has any matches
+@parameter compareFile: This is used to select the file
+that to parse the main file with for matches
 *********************************************************/
 
 int main()
@@ -57,11 +58,12 @@ int main()
 }
 
 /*********************************************************
-This function is used to take a given file and process
-the data into a usable array.
+This function is used to take the given files and process
+the data into usable arrays.
 
-@parameter mainFile: The name of the file to process.
-@parameter compareFile: used to name desired output file
+@parameter mainFile: The name of the main file to process
+@parameter compareFile: The name of the secondary file
+to process
 @return: none
 *********************************************************/
 
@@ -150,13 +152,16 @@ void processFile(char *mainFile, char *compareFile)
 }
 
 /*********************************************************
-This function is used to iterate over the file's data and
-process the local & general complexity of the whole file.
+This function is used to iterate over the main file's
+data and compare it to the secondary file to check for
+the highest matching score
 
-@parameter buff: The file data to be processed.
-@parameter arrLen: The overall size of the file
-@parameter sentenceLen: The length to check local
-complexity.
+@parameter mainBuff: The main file data to be processed.
+@parameter mainArrLen: The overall size of the main file
+@parameter compareBuff: The secondary file data to be
+compared against the main
+@parameter compareArrLen: The overall size of the
+secondary file
 @return: none
 *********************************************************/
 
@@ -166,23 +171,29 @@ void checkMatches(char *mainBuff, int mainArrLen, char *compareBuff, int compare
     int **matchArr = {0};
     int i = 0, j = 0;
 
-
+    //SET UP TIMER FOR FILE
     struct timespec begin, end;
     clock_gettime(CLOCK_REALTIME, &begin);
 
     //ALLOCATE MEMORY FOR COUNT
     matchArr = realloc(matchArr, sizeof(int*) * mainArrLen * 2);
 
+    //ITERATE OVER THE SMALLER FILE AS THIS WILL BE THE COLUMN
     for (i = 0; i < compareArrLen + 1; i++)
     {
+        //ALLOCATE ENOUGH MEMORY FOR THE ROW
         matchArr[i] = malloc(sizeof(int) * mainArrLen * 2);
+
+        //ITERATE OVER THE MAIN FILE CHECKING FOR SMITH-WATERMAN MATCH SCORE
         for (j = 0; j < mainArrLen + 1; j++)
         {
+            //IF IT IS THE FIRST ROW/COLUMN WE KNOW IT MUST BE 0
             if (i == 0 || j == 0)
             {
                 matchArr[i][j] = 0;
             } else
             {
+                //CHECK IF THE ROW AND COLUMN CHARACTER MATCH AND WRITE ITS SCORE
                 if (compareBuff[i-1] == mainBuff[j-1] || compareBuff[i-1] == '?' || mainBuff[j-1] == '?')
                 {
                     matchArr[i][j] = matchArr[i-1][j-1] + 1;
@@ -195,6 +206,7 @@ void checkMatches(char *mainBuff, int mainArrLen, char *compareBuff, int compare
                     //CHECK UP LEFT
                     int upLeftSide = matchArr[i-1][j-1] - 1;
 
+                    //COMPARE TO SEE WHAT THE LARGEST VALUE IS AND WRITE IT
                     if (leftSide > 0 || upSide > 0 || upLeftSide > 0)
                     {
                         if (leftSide > upSide)
@@ -222,6 +234,7 @@ void checkMatches(char *mainBuff, int mainArrLen, char *compareBuff, int compare
         }
     }
 
+    //END CLOCK AND GET TIME
     clock_gettime(CLOCK_REALTIME, &end);
 
     long seconds = end.tv_sec - begin.tv_sec;
@@ -230,19 +243,20 @@ void checkMatches(char *mainBuff, int mainArrLen, char *compareBuff, int compare
 
     printf("time taken %f\n",elapsed);
 
+    //OUTPUT THE MATCH SCORE ARRAY
     createCSV(matchArr, mainArrLen, compareArrLen);
 
+    //FREE MEMORY
     free(matchArr);
 }
 
 /*********************************************************
-This function is used to output the processed complexity
-to a given file.
+This function is used to output the processed match score.
+This is for comparison purpose to ensure the program works
 
-@parameter complexityArr: The file data to be output.
-@parameter arrLen: The overall size of the file.
-@parameter outputFile: The name of the output file
-@parameter genComplexity: The article complexity
+@parameter matchArr: The file data to be output.
+@parameter mainArrLen: The overall row sizes
+@parameter compareArrLen: The overall column sizes
 @return: none
 *********************************************************/
 
